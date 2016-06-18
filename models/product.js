e6eb4198eef95f0f502384245121ee6be73e34c2
Andre Susanto <admin@andresusanto.info>
2016-06-18 22:03:20 +0700

    add constrain to products: they have to be in a category

--- models/product.js
+++ models/product.js
@@ -33,20 +33,24 @@ var productSchema = new mongoose.Schema({
         required: true
     },
     // support for multiple categories products
-    categories: [{
-        type: mongoose.Schema.Types.ObjectId,
+    categories: {
+        type: [mongoose.Schema.Types.ObjectId],
         ref: 'Category',
         validate: {
             // this validator would validate whether reference is valid (existed).
             validator: function(v, cb) {
-                Category.findOne(v, function(err, result){
-                    if (result) cb(true);
-                    else cb(false);
-                })
+                if (v instanceof Array && v.length > 0){
+                    Category.find({_id: {$in: v}}, function(err, result){
+                        if (result) cb(true);
+                        else cb(false);
+                    })
+                } else {
+                  cb(false);
+                }
             },
             message: '{VALUE} is not a valid category!'
         }
-    }]
+    }
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- test/product.js
+++ test/product.js
@@ -11,5 +11,50 @@ const codes = require('../constants/codes');
 
 
 describe('Category Endpoints', function() {
-    
+    var test_categories = [];
+
+    // we need to create some category first
+    before(function(done) {
+        request(config.HTTP_ADDRESS)
+            .put('/category')
+            .send({
+                name: 'Pakaian',
+                description: 'Koleksi celana paling lengkap dan update',
+            })
+            .set('API-KEY', config.TEST_VALID_API_KEY)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+
+                res.body.code.should.equal(codes.OPERATION_SUCCESS);
+                res.body.should.not.have.property('errors');
+
+                res.body.category.description.should.equal('Koleksi celana paling lengkap dan update');
+                res.body.category.name.should.equal('Celana');
+
+                test_categories.push(res.body.category._id);
+
+                request(config.HTTP_ADDRESS)
+                    .put('/category')
+                    .send({
+                        name: 'Pakaian Outdoor',
+                        parent: test_categories[0]
+                    })
+                    .set('API-KEY', config.TEST_VALID_API_KEY)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+
+                        res.body.code.should.equal(codes.OPERATION_SUCCESS);
+                        res.body.should.not.have.property('errors');
+
+                        res.body.category.should.not.have.property('description');
+                        res.body.category.name.should.equal('Pakaian');
+
+                        test_categories.push(res.body.category._id);
+                        return done();
+                    });
+
+            });
+    });
 });
